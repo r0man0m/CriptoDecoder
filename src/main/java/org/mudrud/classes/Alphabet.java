68b77d8e4211afe8ad6dab6bc5319c503ca5a1eb
Roman Mudrud <mudrudr@gmail.com>
2025-07-06 22:04:21 +0300

    create Alphabet.java

---
+++ src/main/java/org/mudrud/classes/Alphabet.java
@@ -0,0 +1,14 @@
+package org.mudrud.classes;
+
+public  class Alphabet {
+    private final static char[] symbols = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    private static Alphabet alphabet;
+
+    private Alphabet(){
+    }
+    public static Alphabet getAlphabet(){
+        return  new Alphabet();
+    }
+
+}
